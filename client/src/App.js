6662bf215ae9d88d59e45b0b952bf5194dfa65cb
yeaminsarder <yeaminsardersr@gmail.com>
2026-07-03 13:36:32 +0600

    tailwind setup

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
 	  <BrowserRouter>
         <Navbar/>
-		<Routes>
+		    <Routes>
           <Route
             path='/'
             element={<Home/>}
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () =>  {
     return (
         <header>
-          <div className="container">
+          <div className="bg-red-500">
             <Link to="/">
               <h1>Home</h1>
             </Link>
