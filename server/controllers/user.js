1f3dd18afa6d5731670e39b7c23bf893b0f839e6
yeaminsarder <yeaminsardersr@gmail.com>
2026-07-12 20:42:18 +0600

    jwt

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,9 @@
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' })
+}
 
 const loginUser = (req, res) => {
     // Implement login logic here
@@ -7,10 +12,10 @@ const loginUser = (req, res) => {
 
 const registerUser = async (req, res) => {
     const { email, password } = req.body;
-
     try {
         const user = await User.register(email, password);
-        res.status(201).json({ email, user });
+        const token = createToken(user._id);
+        res.status(201).json({ email, token });
     } catch (err) {
         res.status(400).json({ error: err.message });
     }
