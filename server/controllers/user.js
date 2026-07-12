39fbace10190f31da32f9f860844d6e28956836c
yeaminsarder <yeaminsardersr@gmail.com>
2026-07-12 21:16:17 +0600

    login logic

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -5,9 +5,15 @@ const createToken = (_id) => {
     return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' })
 }
 
-const loginUser = (req, res) => {
-    // Implement login logic here
-    res.status(200).json({ message: "Login successful" });
+const loginUser = async(req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.status(200).json({ email, token });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 }
 
 const registerUser = async (req, res) => {
--- server/models/user.js
+++ server/models/user.js
@@ -40,4 +40,19 @@ userSchema.statics.register = async function(email, password) {
 	return user
 }
 
+userSchema.statics.login = async function(email, password) {
+	if (!email || !password) {
+		throw new Error('All fields must be filled')
+	}
+	const user = await this.findOne({ email })
+	if (!user) {
+		throw new Error('Incorrect email')
+	}
+	const match = await bcrypt.compare(password, user.password)
+	if (!match) {
+		throw new Error('Incorrect password')
+	}
+	return user
+}
+
 module.exports = mongoose.model('user', userSchema)
