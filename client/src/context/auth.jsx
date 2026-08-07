0949793c82df793c21104e53b59455cf936fb694
yeaminsarder <yeaminsardersr@gmail.com>
2026-08-07 16:03:29 +0600

    fix: reload redirects to home

--- client/src/context/auth.jsx
+++ client/src/context/auth.jsx
@@ -5,9 +5,11 @@ export const AuthContext = createContext()
 export const authReducer = (state, action) => {
     switch (action.type) {
         case 'LOGIN':
-            return { ...state, user: action.payload }
+            return { ...state, user: action.payload, isLoading: false }
         case 'LOGOUT':
-            return { ...state, user: null }
+            return { ...state, user: null, isLoading: false }
+        case 'SET_LOADING':
+            return { ...state, isLoading: action.payload }
         default:
             return state
     }
@@ -16,15 +18,19 @@ export const authReducer = (state, action) => {
 export const AuthContextProvider = ({ children }) => {
     const initialState = {
         user: null,
+        isLoading: true
     }
+    const [state, dispatch] = useReducer(authReducer, initialState)
+
     useEffect(() => {
         const user = JSON.parse(localStorage.getItem('user'))
         if (user) {
             dispatch({ type: 'LOGIN', payload: user })
+        } else {
+            dispatch({ type: 'SET_LOADING', payload: false })
         }
     }, [])
 
-    const [state, dispatch] = useReducer(authReducer, initialState)
     console.log('AuthContext state:', state) // Log the state to see if it's updating correctly
     return (
         <AuthContext.Provider value={{ ...state, dispatch }}>
