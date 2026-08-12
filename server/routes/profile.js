380485c6eb8444650a12825aea5572f3403819d8
Abrar Jahin Zihan <abrarjahinkhanzihan@gmail.com>
2026-08-13 01:59:24 +0600

    feat(routes): register project management endpoints in profile routes

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -6,6 +6,9 @@ import requireAuth from '../middleware/require-auth.js'
 router.use(requireAuth)
 
 router.get('/', profileController.getProfiles)
+router.post('/projects', profileController.addProject)
+router.put('/projects/:projectId', profileController.updateProject)
+router.delete('/projects/:projectId', profileController.deleteProject)
 router.get('/:id', profileController.getProfile)
 router.patch('/:id', profileController.updateProfile)
 router.delete('/:id', profileController.deleteProfile)
