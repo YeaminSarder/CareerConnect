6d1e56b9f57e14123e2addc1993ebbee954f038e
FaisalAlAbdullah <142832715+FaisalAlAbdullah@users.noreply.github.com>
2026-08-17 23:50:28 +0600

    Import SkillRecommendations component in ConnectionsPage

--- client/src/pages/connections.jsx
+++ client/src/pages/connections.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ConnectionManager from '../components/rakibul/ConnectionManager.jsx'
+import SkillRecommendations from '../components/mypart/SkillRecommendations.jsx'
 
 const ConnectionsPage = () => {
 	return <ConnectionManager />
