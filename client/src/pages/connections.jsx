fdfe9a227923e8050c0b207970c4ad4a09de7f00
FaisalAlAbdullah <142832715+FaisalAlAbdullah@users.noreply.github.com>
2026-08-17 23:50:57 +0600

    Update connections.jsx

--- client/src/pages/connections.jsx
+++ client/src/pages/connections.jsx
@@ -3,7 +3,14 @@ import ConnectionManager from '../components/rakibul/ConnectionManager.jsx'
 import SkillRecommendations from '../components/mypart/SkillRecommendations.jsx'
 
 const ConnectionsPage = () => {
-	return <ConnectionManager />
+	return (
+		<div className="container py-4" style={{ maxWidth: '900px' }}>
+			<SkillRecommendations />
+			<div className="mt-4">
+				<ConnectionManager />
+			</div>
+		</div>
+	)
 }
 
 export default ConnectionsPage
