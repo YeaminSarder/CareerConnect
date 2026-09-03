23f0a8f935619057a785f18dfeb4c759865d5320
FaisalAlAbdullah <142832715+FaisalAlAbdullah@users.noreply.github.com>
2026-09-04 01:47:20 +0600

    Update feed.js

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -1,1 +1,11 @@
+import express from 'express'
+import { getFeed } from '../controllers/feed.js'
+import requireAuth from '../middleware/require-auth.js'
+
+const router = express.Router()
+
+router.use(requireAuth)
+router.get('/', getFeed)
+
+export default router
 
