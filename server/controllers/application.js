5d770f7db5b5b1c8900dec507245b2cd8ef36919
yeaminsarder <yeaminsardersr@gmail.com>
2026-09-04 20:53:07 +0600

    fix maching score population when application status change

--- server/controllers/application.js
+++ server/controllers/application.js
@@ -90,13 +90,17 @@ export const updateApplicationStatus = async (req, res) => {
 			id,
 			{ status },
 			{ new: true }
-		).populate('internship').populate('cv', 'title').populate('student', 'name email')
-
+		).populate('internship').populate('cv', 'title').populate({
+				path: 'student',
+				populate: {
+					path: 'profile'
+				}
+			})
 		if (!application) {
 			return res.status(404).json({ error: 'Application record not found' })
 		}
 
-		res.status(200).json(application)
+		res.status(200).json(application.toJSON({ virtuals: true }))
 	} catch (err) {
 		res.status(500).json({ error: err.message })
 	}
